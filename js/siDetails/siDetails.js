86ee169856676d72469b89b607fb0ed76241a270
Liu Ying <181823455@qq.com>
2017-09-28 10:06:26 +0800

    合并代码第一次更新

---
+++ js/siDetails/siDetails.js
@@ -0,0 +1,31 @@
+
+//轮播图
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        slidesPerView: "auto",
+		centeredSlides: true,
+        watchSlidesProgress: !0,
+        paginationClickable: true,
+        spaceBetween: 0,
+        loop: true,
+		loopedSlides :3,
+		initialSlide:1,
+        onProgress: function(a) {
+			var b, c, d;
+			for(b = 0; b < a.slides.length; b++) c = a.slides[b], d = c.progress, scale = 1 - Math.min(Math.abs(.2 * d), 1), es = c.style, es.opacity = 1 - Math.min(Math.abs(d / 2), 1), es.webkitTransform = es.MsTransform = es.msTransform = es.MozTransform = es.OTransform = es.transform = "translate3d(0px,0," + -Math.abs(150 * d) + "px)";
+		},
+		onSetTransition: function(a, b) {
+			for(var c = 0; c < a.slides.length; c++) es = a.slides[c].style, es.webkitTransitionDuration = es.MsTransitionDuration = es.msTransitionDuration = es.MozTransitionDuration = es.OTransitionDuration = es.transitionDuration = b + "ms"
+		}
+    });
+//图片放大    
+    mui.previewImage();
+    
+$(function(){
+	$("footer input").bind("input propertychange",function(){//keycode==13
+		$("footer icon").hide();
+		if($(this).val()==""){
+			$("footer icon").show();
+		}
+	})
+});
